06d62df7a7bc4abbbe36f9aff170c59ca39b2b44
lidannili <lidannili@gmail.com>
2015-10-28 11:49:02 -0500

    First Commit

---
+++ src/module1/LifeExpectancy.java
@@ -0,0 +1,10 @@
+package module1;
+
+import de.fhpotsdam.unfolding.UnfoldingMap;
+import processing.core.PApplet;
+
+public class LifeExpectancy extends PApplet {
+	UnfoldingMap map;
+	Map<String, Float> lifeExpectancyFromCSV;
+
+}
